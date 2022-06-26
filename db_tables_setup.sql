d33b5747e9873b33571dd95c1549194e034413c6
AV-Felipe <aerogrado@hotmail.com>
2022-06-26 12:05:11 -0300

    chore: create sql for accounts and customers tables

---
+++ db_tables_setup.sql
@@ -0,0 +1,29 @@
+
+CREATE TABLE IF NOT EXISTS customers (
+	id uuid UNIQUE,
+	full_name varchar(50) NOT NULL UNIQUE,
+    email varchar(50) NOT NULL UNIQUE,
+    cpf varchar(11) NOT NULL UNIQUE,
+	password varchar(60),
+	created_at timestamp with time zone DEFAULT NOW(),
+	updated_at timestamp with time zone,
+	deleted_at int NOT NULL DEFAULT 0,
+	PRIMARY KEY (id),
+	UNIQUE (full_name, deleted_at)
+);
+
+CREATE TABLE IF NOT EXISTS accounts (
+	id uuid UNIQUE,
+    owner uuid NOT NULL,
+	ac_number varchar(5) NOT NULL,
+    ac_digit varchar(1) NOT NULL,
+   	ag_number varchar(4) NOT NULL,
+    ag_digit varchar(1) NOT NULL,
+	created_at timestamp with time zone DEFAULT NOW(),
+	updated_at timestamp with time zone,
+	deleted_at int NOT NULL DEFAULT 0,
+	PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS accounts
+	ADD CONSTRAINT customer_account_fk0 FOREIGN KEY (owner) REFERENCES customers(id);
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,14 @@
 import dotenv from 'dotenv';
+import { env } from 'process';
 dotenv.config();
 
 const config = {
     PORT: process.env.PORT,
+    POSTGRES_USER: env.POSTGRES_USER,
+    POSTGRES_PASSWORD: env.POSTGRES_PASSWORD,
+    POSTGRES_DB: env.POSTGRES_DB,
+    POSTGRES_PORT: 5432,
+    POSTGRES_HOST: 'localhost'
 };
 
 export { config };
